6dd0f5a9c87248def96d99684f4b6514fd7dd283
Ethan Mendes <37726288+ethanm88@users.noreply.github.com>
2019-05-30 09:16:18 -0400

    Update game.java

--- game.java
+++ game.java
@@ -21,7 +21,7 @@ public class Game {
 	private ArrayList<square> grid;
 	
 	
-	public Game(int x, int y, int m){
+	public Game(int x, int y, int m, int initX, int initY){// constructor
 		grid = new ArrayList<square>();
 		used = new boolean[x][y];
 		mine = new boolean[x][y];
@@ -31,23 +31,28 @@ public class Game {
 		horSize = x;
 		verSize = y;
 		
+		setMines(initX, initY);
+		
 	}
 	
-	public int[] findNext(int x, int y) {
-		while(used[x][y]) {
+	private int[] findNext(int x, int y, int initX, int initY) { //finds next available location
+		while(used[x][y] || Math.abs(x-initX) <=1 ||Math.abs(y-initY)<=1) {
+			
+				
 			if(x == horSize-1) {
 				y++;
 			} 
 			else {
 				x++;
 			}
+			
 				
 		}
 		int[] a = {x, y};
 		return a;
 	}
 	
-	public void randomize(int initX, int initY) {
+	private void randomize(int initX, int initY) {// randomizing location of all mines
 		used[initX][initY] = true;
 		startX = initX;
 		startY = initY;
@@ -55,7 +60,7 @@ public class Game {
 		for(int i = 0; i< numMines; i++) {
 			int xVal = (int)((horSize+1)*Math.random());
 			int yVal = (int)((verSize+1)*Math.random());
-			int pos[] = findNext(xVal, yVal);
+			int pos[] = findNext(xVal, yVal, initX, initY);
 			mine[pos[0]][pos[1]] = true;
 			used[pos[0]][pos[1]] = true;
 
@@ -63,7 +68,7 @@ public class Game {
 		
 	}
 	
-	public void setMines(int initX, int initY) {
+	private void setMines(int initX, int initY) {// sets all mines in random location using randomize
 		randomize(initX, initY);
 		for(int i = 0; i<horSize; i++) {
 			for(int j = 0; j<verSize; j++) {
@@ -83,13 +88,16 @@ public class Game {
 		}
 	}
 	
-	private void floodfill(int x, int y) {
+	private void floodfill(int x, int y) {// method to floodfill to all squares adjacent
 		open[x][y] = true;
 		gameBoard[x][y].dig();
 		
+		if(gameBoard[x][y].getDisplay() !=0)
+			return;
+		
 		for(int i = -1; i<2; i++) {
 			for(int j = -1; j<2; j++) {
-				if(x+i< horSize && x+i>=0 && y+j< verSize && y+j>=0 && !open[x+i][y+j]) {
+				if(x+i< horSize && x+i>=0 && y+j< verSize && y+j>=0 && gameBoard[x+i][y+j].getDisplay() == -1) {
 					floodfill(x+i, y+j);
 				}
 
@@ -97,50 +105,58 @@ public class Game {
 		}
 	}
 	
-	public void clearSquares(int x, int y) {
-		floodfill(x, y);
-	}
-	
-	public void mark(int x, int y) {
-		gameBoard[x][y].mark();
-	}
-	
-	public boolean win() {
-		int numFlagged = 0;
-		int numDug = 0;
-		for(int i = 0; i<horSize; i++) {
-			for(int j = 0; j<verSize; j++) {
-				if(gameBoard[i][j].getDisplay() == -1) {
-					return 0;
-				}
-				else {
-					
-				}
-			}
+	private void clearSquares(int x, int y) {// deals with opening of squares
+		if(gameBoard[x][y].getDisplay() == -1) {
+			floodfill(x, y);
 		}
 	}
 	
-	public int gameState(int clickX, int clickY, boolean mark) {
-		if(!mark) {
-			if(mine[clickX][clickY]) {
+	private void mark(int x, int y) {// marks square
+		gameBoard[x][y].mark();
+	}
+	
+	private boolean win() {// checks if user has won
+		int numFlagged = 0;
+		for(int i = 0; i<horSize; i++) {
+			for(int j = 0; j<verSize; j++) {
+				if(gameBoard[i][j].getDisplay() == -1) {
+					return false;
+				}
+				else if(gameBoard[i][j].getDisplay() == 100){
+					numFlagged++;
+				}
+			}
+		}
+		
+		if(numFlagged == numMines)
+			return true;
+		return false;
+	}
+	
+	public int getNumberAround(int x, int y) {//return the number of mines around square
+		return gameBoard[x][y].getDisplay();
+	}
+	
+	public int gameState(int clickX, int clickY, boolean mark) {//method to be used by the GUI in game
+		if(!mark) {// if turn is to open new square
+			if(mine[clickX][clickY]) {// player loses
 				return -1;
 			}
 			else {
 				clearSquares(clickX, clickY);
 			}
+			
 		}
-		else {
+		else {// if turn is to mark mine
 			mark(clickX, clickY);
 		}
 		
-		if()
+		if(win())// win
+			return 1;
+		
+		return 0;
+		
+		
 	}
-	
-	
-	
-	
-	
 
 }
-
-
